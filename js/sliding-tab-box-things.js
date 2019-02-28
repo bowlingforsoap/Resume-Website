cc875ad50acb6c7ff1d2728654c1debd71621fc9
Vadym Strelchenko <bowlingforsoap@mail.ru>
2019-02-28 18:53:18 +0200

    introduced unity animations on tab switching

--- js/sliding-tab-box-things.js
+++ js/sliding-tab-box-things.js
@@ -1,5 +1,12 @@
 $('.slide a').click(function () {
   $('.slide.active').removeClass('active');
   $(this).closest('.slide').addClass('active');
+  
+  // find the button id
+//  console.log("button #" + $(this).contents("span").text());
+  var btnNum = Number($('.slide.active').contents("span").text());
+  console.log("button #" + btnNum + " was pressed");
+  gameInstance.SendMessage("Main Camera", "ChangeState", btnNum);
+  
   return false;
 });
